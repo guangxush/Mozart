2f80b48ab845db363ec12f5990a7a3495608d141
guangxush <guangxush@163.com>
2018-12-06 09:32:21 +0800

    redivide the data

--- adaptive_learning.py
+++ adaptive_learning.py
@@ -41,8 +41,8 @@ def model1(i):
 
     if not os.path.exists(datafile):
         print("Precess data " + str(i) + "....")
-        data_process.get_part_data(trainfile, testfile, w2v_file, char2v_file, datafile, w2v_k=100, c2v_k=100,
-                                   maxlen=maxlen, left=i)
+        data_process.get_part_train_test_data(trainfile, testfile, w2v_file, char2v_file, datafile, w2v_k=100, c2v_k=100,
+                                              maxlen=maxlen, left=i)
 
     if not os.path.exists("./modfile/" + modelfile):
         print("data has existed: " + datafile)
